bd67b9f2467554bf5eb8eaf99456499cd70f1db4
Michael <Michael@192.168.88.186>
2012-10-28 14:57:56 +0100

    7.4. Final version, alles erledigt

---
+++ src/insel/Insel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insel;
+
+/**
+ *
+ * @author Michael
+ */
+public class Insel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = 2;
+        System.out.println( "Quadrat: " + n * n );
+    }
+}
